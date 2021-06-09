d7d5299bbbad631b19935f8370ea1c4da5eaac78
Administrator <3183848638@qq.com>
2021-06-09 17:57:44 +0800

    动画效果

--- js案例/method.js
+++ js案例/method.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let hours = document.querySelector('.hours');
-    let minute = document.querySelector('.minute');
-    let second = document.querySelector('.second');
-    let input = +new Date('2021-6-6 0:10:50');
+  let hours = document.querySelector('.hours');
+  let minute = document.querySelector('.minute');
+  let second = document.querySelector('.second');
+  let input = +new Date('2021-6-9 0:10:50');
 
-    function countDown() {
+  function countDown() {
 
-        let now = +new Date();
-        let times = parseInt((input - now) / 1000 + '');
-        let d = parseInt(times / 60 / 60 / 24 + '');
-        d = d < 10 ? '0' + d : d;
-        let h = parseInt(times / 60 / 60 % 24 + '');
-        h = h < 10 ? '0' + h : h;
-        hours.innerHTML = h;
-        let m = parseInt(times / 60 % 60 + '');
-        m = m < 10 ? '0' + m : m;
-        minute.innerHTML = m;
-        let s = parseInt(times % 60 + '');
-        s = s < 10 ? '0' + s : s;
-        second.innerHTML = s;
-        if (times <= 0) {
-            clearInterval(timers);
-        }
-        return d + ':' + h + ':' + m + ':' + s;
+    let now = +new Date();
+    let times = parseInt((input - now) / 1000 + '');
+    let d = parseInt(times / 60 / 60 / 24 + '');
+    d = d < 10 ? '0' + d : d;
+    let h = parseInt(times / 60 / 60 % 24 + '');
+    h = h < 10 ? '0' + h : h;
+    hours.innerHTML = h;
+    let m = parseInt(times / 60 % 60 + '');
+    m = m < 10 ? '0' + m : m;
+    minute.innerHTML = m;
+    let s = parseInt(times % 60 + '');
+    s = s < 10 ? '0' + s : s;
+    second.innerHTML = s;
+    if (times <= 0) {
+      clearInterval(timers);
     }
+    return d + ':' + h + ':' + m + ':' + s;
+  }
 
-    countDown();
-    let timers = setInterval(countDown, 1000);
+  countDown();
+  let timers = setInterval(countDown, 1000);
 
 
 });
